c2ecf1c8aa60e35858beeb1aa4811a42a8803733
Bruna Santos <brunasan7os@gmail.com>
2025-03-25 19:49:52 -0300

    Implementada funcionalidade de exibição da dica da palavra

--- jogoDaForca.ConsoleApp/Program.cs
+++ jogoDaForca.ConsoleApp/Program.cs
@@ -7,9 +7,23 @@
 
             while (true)
             {
+                string palavraSecreta = "MELANCIA";
+
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+                {
+                    //acessar o array no indice 0
+                    letrasEncontradas[caractere] = '_';
+                }
+
+                string dicadaPalavra = String.Join(" ", letrasEncontradas);
+
                 Console.Clear();
                 Console.WriteLine("--------------------------------------");
                 Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Palavra secreta: " + dicadaPalavra);
                 Console.WriteLine("--------------------------------------");
 
                 Console.WriteLine("Digite uma letra: ");
@@ -18,7 +32,8 @@
 
                 Console.WriteLine(chute);
                 Console.ReadLine();
-            
-        }
+
+            }
+        }    
     }
 }
